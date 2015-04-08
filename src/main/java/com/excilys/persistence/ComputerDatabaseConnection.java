9eb530c828c15e9c70dc5a5e33c6fea3e381ddbd
jclegras <jclegras@excilys.com>
2015-04-08 15:15:44 +0200

    refactor(computer-database): refactoring de la factory à connexions
    Suppression de ThreadLocal.

--- src/main/java/com/excilys/persistence/ComputerDatabaseConnection.java
+++ src/main/java/com/excilys/persistence/ComputerDatabaseConnection.java
@@ -1,29 +1,19 @@
 package com.excilys.persistence;
 
-import com.excilys.exception.PersistenceException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.sql.Connection;
-import java.sql.SQLException;
+import com.excilys.exception.PersistenceException;
 
 @Component
 public class ComputerDatabaseConnection {
 
 	@Autowired
 	private BasicDataSource dataSource;
-
-	private final ThreadLocal<Connection> CONNECTION = new ThreadLocal<Connection>() {
-		@Override
-	    protected Connection initialValue() {
-	        try {
-				return dataSource.getConnection();
-			} catch (SQLException e) {
-				throw new PersistenceException(e);
-			}
-	    }
-	};
 
 	/**
 	 * @return A connection to the database
@@ -36,6 +26,10 @@ public class ComputerDatabaseConnection {
 	 * Retrieve a connection for the current thread.
 	 */
 	private Connection getConnection() {
-		return CONNECTION.get();
+		try {
+			return dataSource.getConnection();
+		} catch (SQLException e) {
+			throw new PersistenceException(e);
+		}
 	}
 }
