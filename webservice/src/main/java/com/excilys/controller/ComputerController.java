3491f4cd827205ef87105a0667b9f188fcb9c718
jclegras <jclegras@excilys.com>
2015-04-27 14:46:24 +0200

    feat(computer-database): webservice avec Jackson
    Jackson et JAXB dans le classpath pour permettre une sérialisation automatique vers JSON.
    Fasterxml dans le classpath pour permettre une sérialisation automatique vers XML.

---
+++ webservice/src/main/java/com/excilys/controller/ComputerController.java
@@ -0,0 +1,29 @@
+package com.excilys.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.excilys.dto.ComputerDTO;
+import com.excilys.mapper.MapperDTO;
+import com.excilys.model.Computer;
+import com.excilys.service.IComputerService;
+
+@RestController
+@RequestMapping("/computer")
+public class ComputerController {
+	@Autowired
+	private IComputerService computerService;
+	@Autowired
+	private MapperDTO<Computer, ComputerDTO> mapper;
+
+	@RequestMapping(method = RequestMethod.GET, value = "/all", produces = {
+			MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
+	public List<ComputerDTO> test() {
+		return mapper.modelsToDto(computerService.getAll());
+	}
+}
